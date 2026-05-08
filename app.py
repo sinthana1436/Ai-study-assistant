f75a2c7db47599b9b330cc5a185e5246f3db3559
sinthana1436 <sinthsinthanaa@gmail.com>
2026-05-08 09:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Studyz")
 st.write("your ai mentor")
 name = st.text_input("what is your good name")
-if st.button("click")
-st.write("welcome you!")
+if st.button("click"):
+   st.write("welcome you!")
