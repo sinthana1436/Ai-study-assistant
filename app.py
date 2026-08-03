c4daad155e56bfad7e2b2ab4d6a77384e7444741
sinthana1436 <sinthsinthanaa@gmail.com>
2026-08-03 23:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import date
 
 genai.configure(api_key="AIzaSyALYKsMhqAhLopx9IXf1jFkeTpvx3KPJd0")
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 
