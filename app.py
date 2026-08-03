d490c4eb29b5f91cc4a9be34c361ba95ddd72e59
sinthana1436 <sinthsinthanaa@gmail.com>
2026-08-03 22:29:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,28 +8,5 @@ if st.button("click"):
    if name:
       st.success(f"hi {name}♡ welcome to studyz")
    else:
-         st.warning("please give your name")
-st.divider()
-def answer_question(question):
-   api_key=
-   URL =
-   headers ={"authorisation":f"bearer{api_key}"}
-   data={"questions":question}
-   response=requests.post(URL,headers=headers,json=data)
-   result=response.json()
-   try:
-      return result["answer"]
-   except:
-      return "i don't know"
-
-    
-question = st.text_input("ask yours doublt!")
-if st.button("submit"):
-   if question:
-      answer = answer_question(question)
-
-      st.success(answer)
-
-   else:
-      st.warning("Please enter a question.")
+      st.warning("please give your name")
    
